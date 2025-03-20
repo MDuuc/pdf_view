e85f4b1ee3eda7cb36904ca7614bc652c858e6e2
Ta MInh Duc <toikobiet2.0@gmail.com>
2025-03-20 15:43:21 +0700

    remove unuse pakage

--- view_pdf/macos/Flutter/GeneratedPluginRegistrant.swift
+++ view_pdf/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import file_picker
 import file_selector_macos
 import open_file_mac
 import path_provider_foundation
-import pdf_render
 import pdfx
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -19,6 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SwiftPdfRenderPlugin.register(with: registry.registrar(forPlugin: "SwiftPdfRenderPlugin"))
   PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
 }
